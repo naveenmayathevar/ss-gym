a86cfa045af2e71f4789696fbafb29f39acac93e
Naveen Mayathevar <naveenmayathevar@gmail.com>
2026-03-10 19:14:41 -0700

    Added missing upgrade import

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import create_app
+from flask_migrate import upgrade  # <--- You need this line right here!
 
 app = create_app()
+
 with app.app_context():
-    from app import db
-    db.create_all()
+    upgrade()
 
 if __name__ == '__main__':
     # Runs the app on port 5000 and allows connections from anywhere (0.0.0.0)
