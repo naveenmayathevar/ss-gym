21eda81415447385611e4dd257b484c994615a46
Naveen Mayathevar <naveenmayathevar@gmail.com>
2026-03-10 19:08:12 -0700

    Added auto-database creation to bypass Render shell paywall

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
 
 app = create_app()
+with app.app_context():
+    from app import db
+    db.create_all()
 
 if __name__ == '__main__':
     # Runs the app on port 5000 and allows connections from anywhere (0.0.0.0)
